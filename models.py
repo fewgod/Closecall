2fb31d13577bf01cb408bfe9f187828c3305657b
Few <phuklagod@hotmail.com>
2017-10-06 03:07:43 +0700

    tried to randomly gen block -FAIL

--- models.py
+++ models.py
@@ -17,15 +17,30 @@ class World:
         self.width = width
         self.height = height
         self.line_sprite = arcade.Sprite('images/line.png') #insert deathline picture
+        self.blocks = []
         Spawn_Lane = randint(1,3)
         if(Spawn_Lane == 1):
-            self.block = Block(100, 500)
+            block = Block(100, 700)
+            self.blocks.append(block)
         if(Spawn_Lane == 2):
-            self.block = Block(300, 500)
+            block = Block(300, 700)
+            self.blocks.append(block)
         if(Spawn_Lane == 3):
-            self.block = Block(500, 500)
- 
-    #def randomlane(self):
+            block = Block(500, 700)
+            self.blocks.append(block)
 
     def update(self,delta):
-        self.block.update(delta)
+        if(randint(1,100) == 1):
+            block = arcade.Sprite("images/block.png")
+            Spawn_Lane = randint(1,3)
+            if(Spawn_Lane == 1):
+                block = Block(100, 700)
+                self.blocks.append(block)
+            if(Spawn_Lane == 2):
+                block = Block(300, 700)
+                self.blocks.append(block)
+            if(Spawn_Lane == 3):
+                block = Block(500, 700)
+                self.blocks.append(block)
+        for block in world.blocks:
+            self.block.update(delta)
