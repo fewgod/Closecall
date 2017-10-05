1953170e9ea585afdc93a4280676df8fd96f85e4
Few <phuklagod@hotmail.com>
2017-10-06 04:07:05 +0700

    press space to gain score by distance,bounce back

--- Closecall.py
+++ Closecall.py
@@ -24,15 +24,30 @@ class ClosecallWindow(arcade.Window):
         super().__init__(width, height)
         arcade.set_background_color(arcade.color.SKY_BLUE)
         self.block_sprite = arcade.Sprite('images/block.png')
-        #self.background = arcade.load_texture("images/background.jpg") Not working
+        #self.background = arcade.load_texture("images/background.jpg") หาทางใส่bg
         self.world = World(width, height)
+        self.score = 0
+
+        self.line_sprite = arcade.Sprite('images/line.png') #insert deathline picture
+        self.line_sprite.set_position(300,100) #set deathline position
+        self.Lane_line_sprite = arcade.Sprite('images/laneline.png') #insert laneline picture
+        self.Lane_line2_sprite = arcade.Sprite('images/laneline.png')
+        self.Lane_line_sprite.set_position(200,450) #set laneline position
+        self.Lane_line2_sprite.set_position(400,450)
 
     def on_key_press(self, key, key_modifiers):
         self.world.on_key_press(key, key_modifiers)
-        
+
     def on_draw(self):
         arcade.start_render()
         self.block_sprite.draw()
+        output = f"Score: {self.world.score}" #ทำป้ายคะแนน
+        self.score_text = arcade.create_text(output, arcade.color.BLACK, 14)
+        arcade.render_text(self.score_text, 10, 20)
+
+        self.line_sprite.draw() #ใส่ฉากหลังแบบไม่ใช่bg
+        self.Lane_line_sprite.draw()
+        self.Lane_line2_sprite.draw()
     
     def update(self, delta):
         self.world.update(delta)
--- models.py
+++ models.py
@@ -3,7 +3,6 @@ import arcade.key
 from random import randint
 Player_Line_X = 300
 Player_Line_Y = 100
-
 class Block:
     def __init__(self, x, y):
         self.x = x
@@ -13,6 +12,8 @@ class Block:
         self.y -= 5
         #if(self.y<100):
             #insert game over function
+        if(self.y<200): #ตอนนี้ทำแค่เลยเส้นแล้วขึ้นไปบนใหม่
+            self.y = 700
 
 
 class World:
@@ -20,6 +21,7 @@ class World:
         self.width = width
         self.height = height
         self.line_sprite = arcade.Sprite('images/line.png') #insert deathline picture
+        self.score = 0 #Initial Score = 0
         Spawn_Lane = randint(1,3)
         if(Spawn_Lane == 1):
             self.block = Block(100, 700)
@@ -29,8 +31,11 @@ class World:
             self.block = Block(500, 700)
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.SPACE:
-            self.block.kill()
-    #def randomlane(self):
+            if(round(780 -self.block.y)>=550): #ประมาณว่าถ้ากดใกล้เส้นตายมากๆจะได้คะแนนพิเศษ
+                self.score += 200
+            else:
+                self.score += round((780 -self.block.y) *0.13)
+    #def randomlane(self): #หาทางใส่ฟังก์ชั่นสุ่มสร้างบล็อคมาเรื่อยๆ
 
     def update(self,delta):
         self.block.update(delta)
